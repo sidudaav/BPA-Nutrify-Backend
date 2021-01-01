9ef58138a6a3705caaf19b137e10d4c410b4fe70
Sid Jha <sidjha03@gmail.com>
2020-12-31 20:44:20 -0500

    edited meal model

--- models/meal.model.js
+++ models/meal.model.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const mealSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     foods: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Food',
+            required: true,
         },
     ],
 });
